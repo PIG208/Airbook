1fba031cee397a0d37e04822213b86e3f6f9f8f8
PIG208 <359101898@qq.com>
2021-04-22 18:45:37 +0800

    fixup! Initial commit.

--- scripts/sql/init.sql
+++ scripts/sql/init.sql
@@ -1,6 +1,6 @@
 SELECT 'Dropping existing airbook database and user.' AS '';
 DROP USER IF EXISTS 'airbook_admin'@'localhost';
-DROP DATABASE airbook;
+DROP DATABASE IF EXISTS airbook;
 
 SELECT 'Creating airbook_admin and the database.' AS '';
 CREATE USER 'airbook_admin'@'localhost' IDENTIFIED BY 'Airbook_admin_x7fo1a';
