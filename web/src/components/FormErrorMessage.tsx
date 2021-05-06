327bac0d33715bef97cb5871d9b9127565486d35
PIG208 <359101898@qq.com>
2021-05-06 18:05:47 +0800

    style: Remove FontErrorMessage.css.

--- web/src/components/FormErrorMessage.tsx
+++ web/src/components/FormErrorMessage.tsx
@@ -1,16 +1,11 @@
 import Form from "react-bootstrap/Form";
-import "../assets/FormErrorMessage.css";
 
 const FormErrorMessage = (props: { message?: string }) => {
   if (props.message === undefined || props.message.length === 0) {
     return <div></div>;
   }
   return (
-    <Form.Control.Feedback
-      type="invalid"
-      className="form-error-message"
-      style={{ display: "block" }}
-    >
+    <Form.Control.Feedback type="invalid" style={{ display: "block" }}>
       {props.message}
     </Form.Control.Feedback>
   );
